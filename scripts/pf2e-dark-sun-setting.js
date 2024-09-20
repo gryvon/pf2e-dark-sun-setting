43c4de7708e3518a4dc9f24d7515833da0d0b37b
ClusterPhunk <58378641+gryvon@users.noreply.github.com>
2024-09-20 00:48:18 -0700

    2024-09-20

---
+++ scripts/pf2e-dark-sun-setting.js
@@ -0,0 +1,19 @@
+// import { ActorPF2e } from "@actor";
+
+Hooks.once('init', () => {
+	console.debug("pf2e-dark-sun-setting | Initialized.")
+
+});
+
+Hooks.once("ready", async () => {
+	//phase = Phase.SETUP;
+	const campaignFeatSections = game.settings.get("pf2e", "campaignFeatSections");
+	campaignFeatSections.push({
+		id: "pf2e-dark-sun-setting-wild-talents",
+		label: "Wild Talents",
+		supported: ["ancestry"],
+		slots: [1, 5, 9, 13, 17]
+	});
+	game.settings.set("pf2e", "campaignFeatSections", campaignFeatSections);
+
+})
