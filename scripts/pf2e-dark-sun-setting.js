eea58a5c168a3671a3cdee2c92fbf9bfd05cb450
ClusterPhunk <58378641+gryvon@users.noreply.github.com>
2024-09-21 12:25:02 -0700

    2024-09-21

--- scripts/pf2e-dark-sun-setting.js
+++ scripts/pf2e-dark-sun-setting.js
@@ -1,6 +1,7 @@
 Hooks.once('init', () => {
 	loadTemplates([
 		'modules/pf2e-dark-sun-setting/templates/rest-for-the-night.hbs',
+		'modules/pf2e-dark-sun-setting/templates/party-resources.hbs',
 	]);
 	console.debug("pf2e-dark-sun-setting | Initialized.")
 });
@@ -112,4 +113,57 @@ function sendChatMessage(character, content) {
         speaker: { actor: character._id },  // Send message as the character
         content: content  // The message content
     });
+}
+
+Hooks.on("renderPartySheetPF2e", async (party, html, actor) => {
+	console.log("Party sheet detected!")
+
+	// Path to custom template.
+	const tpl = 'modules/pf2e-dark-sun-setting/templates/party-resources.hbs';
+
+	// Render the data into the template's handlebars.
+	const myHtml = await renderTemplate(tpl, { actor });
+
+	// Find the inventory portion.
+	const target = $(html).find('div.summary-data');
+
+	target.append(myHtml);
+
+})
+
+
+////////////////////////////
+
+async function adjustmentFoodDialog(party) {
+	let foodAdjustment;
+	try {
+	  foodAdjustment = await foundry.applications.api.DialogV2.prompt({
+	    window: { title: "How much would you like to adjust the Food Points?" },
+	    content: '<input name="adj" type="number" autofocus>',
+	    ok: {
+	      label: "Submit",
+	      callback: (event, button, dialog) => button.form.elements.guess.valueAsNumber
+	    }
+	  });
+	} catch {
+	  //console.log("User did not make a guess.");
+	  return;
+	}
+	adjustFood(party, foodAdjustment);
+}
+
+Hooks.on("darksun.adjustmentFoodDialog"), async (party) => {
+  await adjustmentFoodDialog(party)
+}
+
+async function adjustFood(party, value) {
+  if (!party.hasOwnProperty('darkSun')) {
+    party.system.darkSun = {
+      foodPoints: 0,
+      resourcePoints: 0
+    }
+  }
+
+  let newFoodValue = (party.darkSun.system.foodPoints || 0) + value;
+  await party.update({ "darkSun.system.foodPoints": newFoodValue });
 }
